10db821cd7ac8eb6a64fc4e061488014e5273448
Soraya Sabrina <sorayasabrina@Sorayas-MacBook-Air.local>
2022-09-13 11:53:23 +0700

    initial commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Soraya Sabrina',
+        'npm': '2106651061',
+    }
+    return render(request, "katalog.html", context)
